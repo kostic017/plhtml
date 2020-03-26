a362c1a776f0ff4c99cf64d8d2cbf4416018cd5e
kostic017 <kostic017@mail.com>
2020-03-26 18:15:20 +0100

    Tweak scanner test

--- main_test.go
+++ main_test.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "github.com/stretchr/testify/assert"
-    "strconv"
-    "strings"
-    "testing"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
+	"testing"
 
-    "./parser"
-    "./scanner"
-    "./token"
-    "./util"
+	"./parser"
+	"./scanner"
+	"./token"
+	"./util"
 )
 
 func TestScanner(t *testing.T) {
-    tokens := scan("./tests/examples/fibonacci.html")
-    compare(t, "fibonacci.scanner", tokensToString(tokens))
+	tokens := scan("./tests/examples/fibonacci.html")
+	compare(t, "fibonacci.scanner", tokensToString(tokens))
 }
 
 func TestParser(t *testing.T) {
-    tokens := scan("./tests/examples/fibonacci.html")
-    myParser := parser.New()
-    prgNode := myParser.Parse(tokens)
-    compare(t, "fibonacci.parser", prgNode.ToString())
+	tokens := scan("./tests/examples/fibonacci.html")
+	myParser := parser.New()
+	prgNode := myParser.Parse(tokens)
+	compare(t, "fibonacci.parser", prgNode.ToString())
 }
 
 func scan(file string) []scanner.Token {
-    source := util.ReadFile(file)
-    myScanner := scanner.New()
-    return myScanner.Scan(source)
+	source := util.ReadFile(file)
+	myScanner := scanner.New()
+	return myScanner.Scan(source)
 }
 
 func compare(t *testing.T, testName string, actual string) {
-    expected := util.ReadFile("./tests/" + testName + ".expected")
+	expected := util.ReadFile("./tests/" + testName + ".expected")
 
-    if strings.TrimSpace(expected) != strings.TrimSpace(actual) {
-        util.WriteFile("./tests/"+testName+".actual", actual)
-    }
+	if strings.TrimSpace(expected) != strings.TrimSpace(actual) {
+		util.WriteFile("./tests/"+testName+".actual", actual)
+	}
 
-    assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(actual), "These two should be the same.")
+	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(actual), "These two should be the same.")
 }
 
 func tokensToString(tokens []scanner.Token) string {
 
-    result := ""
-    for _, tok := range tokens {
+	result := ""
+	for _, tok := range tokens {
 
-        if tok.Type == token.EOF {
-            break
-        }
+		if tok.Type == token.EOF {
+			break
+		}
 
-        var value string
-        switch tok.Type {
-        case token.Identifier, token.StringConst:
-            value = tok.StrVal
-        case token.IntConst:
-            value = strconv.Itoa(tok.IntVal)
-        case token.RealConst:
-            value = util.FloatToString(tok.RealVal)
-        case token.BoolConst:
-            value = strconv.FormatBool(tok.BoolVal)
-        }
+		var value string
+		switch tok.Type {
+		case token.Identifier, token.StringConst:
+			value = tok.StrVal
+		case token.IntConst:
+			value = strconv.Itoa(tok.IntVal)
+		case token.RealConst:
+			value = util.FloatToString(tok.RealVal)
+		case token.BoolConst:
+			value = strconv.FormatBool(tok.BoolVal)
+		}
 
-        result += fmt.Sprintf("(%s,%d,%d,%s)\n", tok.Type.String(), tok.Line, tok.Column, value)
+		result += fmt.Sprintf("%s|%d|%d|%s|\n", tok.Type.String(), tok.Line, tok.Column, value)
 
-    }
+	}
 
-    return result
+	return result
 }
