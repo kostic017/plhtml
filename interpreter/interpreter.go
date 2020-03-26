7c2349a5dde59a6772aa70fb9780b6256eca0dda
kostic017 <kostic017@mail.com>
2020-03-26 10:56:48 +0100

    WriteStmt without implicit \n

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,15 +1,16 @@
 package interpreter
 
 import (
-    "../ast"
-    "../logger"
-    "../token"
-    "../util"
-    "bufio"
-    "fmt"
-    "go/constant"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"go/constant"
+	"os"
+	"strconv"
+
+	"../ast"
+	"../logger"
+	"../token"
+	"../util"
 )
 
 type TokenType = token.Type
@@ -17,19 +18,19 @@ type TokenType = token.Type
 var myLogger = logger.New("INTERPRETER")
 
 func SetLogLevel(level logger.LogLevel) {
-    myLogger.SetLevel(level)
+	myLogger.SetLevel(level)
 }
 
 type Interpreter struct {
-    in        *bufio.Scanner
-    variables map[string]constant.Value
+	in        *bufio.Scanner
+	variables map[string]constant.Value
 }
 
 func New() *Interpreter {
-    interp := new(Interpreter)
-    interp.in = bufio.NewScanner(os.Stdin)
-    interp.variables = make(map[string]constant.Value)
-    return interp
+	interp := new(Interpreter)
+	interp.in = bufio.NewScanner(os.Stdin)
+	interp.variables = make(map[string]constant.Value)
+	return interp
 }
 
 /**********************************************
@@ -37,264 +38,264 @@ func New() *Interpreter {
  **********************************************/
 
 func (interp *Interpreter) VisitBinaryOpExpr(node ast.BinaryOpExprNode) interface{} {
-    leftValue := node.LeftExpr.Accept(interp).(constant.Value)
-    rightValue := node.RightExpr.Accept(interp).(constant.Value)
-    myLogger.Debug("%s %s %s", leftValue.String(), node.Operator.String(), rightValue.String())
+	leftValue := node.LeftExpr.Accept(interp).(constant.Value)
+	rightValue := node.RightExpr.Accept(interp).(constant.Value)
+	myLogger.Debug("%s %s %s", leftValue.String(), node.Operator.String(), rightValue.String())
 
-    if node.Operator == token.Plus && (isStr(leftValue) || isStr(rightValue)) {
-        return strcat(leftValue, rightValue)
-    }
+	if node.Operator == token.Plus && (isStr(leftValue) || isStr(rightValue)) {
+		return strcat(leftValue, rightValue)
+	}
 
-    if isNum(leftValue) && isNum(rightValue) {
-        return opsWithNums(leftValue, node.Operator, rightValue)
-    }
+	if isNum(leftValue) && isNum(rightValue) {
+		return opsWithNums(leftValue, node.Operator, rightValue)
+	}
 
-    //if isBool(leftValue) && isBool(rightValue) {
-    //    return opsWithBools(leftValue, node.Operator, rightValue)
-    //}
+	//if isBool(leftValue) && isBool(rightValue) {
+	//    return opsWithBools(leftValue, node.Operator, rightValue)
+	//}
 
-    panic("Binary operator " + node.Operator.String() + " is not supported for operands of given types.")
+	panic("Binary operator " + node.Operator.String() + " is not supported for operands of given types.")
 }
 
 func (interp *Interpreter) VisitBoolConst(node ast.BoolConstNode) interface{} {
-    myLogger.Debug(fmt.Sprintf("Bool %t", node.Value))
-    return constant.MakeBool(node.Value)
+	myLogger.Debug(fmt.Sprintf("Bool %t", node.Value))
+	return constant.MakeBool(node.Value)
 }
 
 func (interp *Interpreter) VisitControlFlowStmt(node ast.ControlFlowStmtNode) {
-    myLogger.Debug(node.Type.String())
-    switch node.Type {
-    case token.If:
-        if constant.BoolVal(node.Condition.Accept(interp).(constant.Value)) {
-            for _, stmt := range node.Statements {
-                stmt.Accept(interp)
-            }
-        }
-        break
-    case token.While:
-        for constant.BoolVal(node.Condition.Accept(interp).(constant.Value)) {
-            for _, stmt := range node.Statements {
-                stmt.Accept(interp)
-            }
-        }
-    }
+	myLogger.Debug(node.Type.String())
+	switch node.Type {
+	case token.If:
+		if constant.BoolVal(node.Condition.Accept(interp).(constant.Value)) {
+			for _, stmt := range node.Statements {
+				stmt.Accept(interp)
+			}
+		}
+		break
+	case token.While:
+		for constant.BoolVal(node.Condition.Accept(interp).(constant.Value)) {
+			for _, stmt := range node.Statements {
+				stmt.Accept(interp)
+			}
+		}
+	}
 }
 
 func (interp *Interpreter) VisitIdentifier(node ast.IdentifierNode) interface{} {
-    interp.checkVarDecl(node.Name)
-    value := interp.variables[node.Name].(constant.Value)
-    myLogger.Debug("%s: %s", node.Name, value.String())
-    return value
+	interp.checkVarDecl(node.Name)
+	value := interp.variables[node.Name].(constant.Value)
+	myLogger.Debug("%s: %s", node.Name, value.String())
+	return value
 }
 
 func (interp *Interpreter) VisitIntConst(node ast.IntConstNode) interface{} {
-    return constant.MakeInt64(int64(node.Value))
+	return constant.MakeInt64(int64(node.Value))
 }
 
 func (interp *Interpreter) VisitMainFunc(node ast.MainFuncNode) {
-    for _, stmt := range node.Statements {
-        stmt.Accept(interp)
-    }
+	for _, stmt := range node.Statements {
+		stmt.Accept(interp)
+	}
 }
 
 func (interp *Interpreter) VisitProgram(node ast.ProgramNode) {
-    node.Body.Accept(interp)
+	node.Body.Accept(interp)
 }
 
 func (interp *Interpreter) VisitProgramBody(node ast.ProgramBodyNode) {
-    node.MainFunc.Accept(interp)
+	node.MainFunc.Accept(interp)
 }
 
 func (interp *Interpreter) VisitReadStmt(node ast.ReadStmtNode) {
-    // TODO correct type
-    interp.checkVarDecl(node.Identifier.Name)
-    interp.in.Scan()
+	// TODO correct type
+	interp.checkVarDecl(node.Identifier.Name)
+	interp.in.Scan()
 
-    val, err := util.StrToInt64(interp.in.Text())
-    util.Check(err)
+	val, err := util.StrToInt64(interp.in.Text())
+	util.Check(err)
 
-    interp.variables[node.Identifier.Name] = constant.MakeInt64(val)
+	interp.variables[node.Identifier.Name] = constant.MakeInt64(val)
 }
 
 func (interp *Interpreter) VisitRealConst(node ast.RealConstNode) interface{} {
-    return constant.MakeFloat64(node.Value)
+	return constant.MakeFloat64(node.Value)
 }
 
 func (interp *Interpreter) VisitStringConst(node ast.StringConstNode) interface{} {
-    return constant.MakeString(node.Value)
+	return constant.MakeString(node.Value)
 }
 
 func (interp *Interpreter) VisitUnaryExpr(node ast.UnaryExprNode) interface{} {
-    exprValue := node.Expr.Accept(interp).(constant.Value)
+	exprValue := node.Expr.Accept(interp).(constant.Value)
 
-    switch node.Operator {
-    case token.Minus:
-        if exprValue.Kind() == constant.Int {
-            exprVal, _ := constant.Int64Val(exprValue)
-            return constant.MakeInt64(-exprVal)
-        } else if exprValue.Kind() == constant.Float {
-            exprVal, _ := constant.Float64Val(exprValue)
-            return constant.MakeFloat64(-exprVal)
-        }
-        break
-    case token.Exclamation:
-        if exprValue.Kind() == constant.Bool {
-            return !constant.BoolVal(exprValue)
-        }
-    }
+	switch node.Operator {
+	case token.Minus:
+		if exprValue.Kind() == constant.Int {
+			exprVal, _ := constant.Int64Val(exprValue)
+			return constant.MakeInt64(-exprVal)
+		} else if exprValue.Kind() == constant.Float {
+			exprVal, _ := constant.Float64Val(exprValue)
+			return constant.MakeFloat64(-exprVal)
+		}
+		break
+	case token.Exclamation:
+		if exprValue.Kind() == constant.Bool {
+			return !constant.BoolVal(exprValue)
+		}
+	}
 
-    panic("Unary operator " + node.Operator.String() + " is not supported for given types.")
+	panic("Unary operator " + node.Operator.String() + " is not supported for given types.")
 }
 
 func (interp *Interpreter) VisitVarAssign(node ast.VarAssignNode) {
-    // TODO correct type
-    interp.checkVarDecl(node.Identifier.Name)
-    value := node.Value.Accept(interp).(constant.Value)
-    myLogger.Debug("%s = %s", node.Identifier.Name, value.String())
-    interp.variables[node.Identifier.Name] = value
+	// TODO correct type
+	interp.checkVarDecl(node.Identifier.Name)
+	value := node.Value.Accept(interp).(constant.Value)
+	myLogger.Debug("%s = %s", node.Identifier.Name, value.String())
+	interp.variables[node.Identifier.Name] = value
 }
 
 func (interp *Interpreter) VisitVarDecl(node ast.VarDeclNode) {
-    // TODO correct type and default value
-    myLogger.Debug("%s %s", node.Type.Name, node.Identifier.Name)
-    interp.variables[node.Identifier.Name] = constant.MakeInt64(0)
+	// TODO correct type and default value
+	myLogger.Debug("%s %s", node.Type.Name, node.Identifier.Name)
+	interp.variables[node.Identifier.Name] = constant.MakeInt64(0)
 }
 
 func (interp *Interpreter) VisitWriteStmt(node ast.WriteStmtNode) {
-    exprValue := node.Value.Accept(interp).(constant.Value)
-    if exprValue.Kind() == constant.String {
-        fmt.Println(constant.StringVal(exprValue))
-    } else {
-        panic("You can print strings only.")
-    }
+	exprValue := node.Value.Accept(interp).(constant.Value)
+	if exprValue.Kind() == constant.String {
+		fmt.Print(constant.StringVal(exprValue))
+	} else {
+		panic("You can print strings only.")
+	}
 }
 
 func (interp *Interpreter) checkVarDecl(name string) {
-    if _, declared := interp.variables[name]; !declared {
-        panic("Variable " + name + " was not declared before it was used.")
-    }
+	if _, declared := interp.variables[name]; !declared {
+		panic("Variable " + name + " was not declared before it was used.")
+	}
 }
 
 func isNum(val constant.Value) bool {
-    return val.Kind() == constant.Int || val.Kind() == constant.Float
+	return val.Kind() == constant.Int || val.Kind() == constant.Float
 }
 
 func isStr(val constant.Value) bool {
-    return val.Kind() == constant.String
+	return val.Kind() == constant.String
 }
 
 func isBool(val constant.Value) bool {
-    return val.Kind() == constant.Bool
+	return val.Kind() == constant.Bool
 }
 
 func strcat(leftValue constant.Value, rightValue constant.Value) constant.Value {
-    if leftValue.Kind() == constant.String && rightValue.Kind() == constant.String {
-        return constant.MakeString(constant.StringVal(leftValue) + constant.StringVal(rightValue))
-    }
-    if leftValue.Kind() == constant.String {
-        return strcatImplicitConversion(leftValue, rightValue, true)
-    }
-    if rightValue.Kind() == constant.String {
-        return strcatImplicitConversion(rightValue, leftValue, false)
-    }
-    panic("Could not perform string concatenation.")
+	if leftValue.Kind() == constant.String && rightValue.Kind() == constant.String {
+		return constant.MakeString(constant.StringVal(leftValue) + constant.StringVal(rightValue))
+	}
+	if leftValue.Kind() == constant.String {
+		return strcatImplicitConversion(leftValue, rightValue, true)
+	}
+	if rightValue.Kind() == constant.String {
+		return strcatImplicitConversion(rightValue, leftValue, false)
+	}
+	panic("Could not perform string concatenation.")
 }
 
 func strcatImplicitConversion(stringValue constant.Value, otherValue constant.Value, ordered bool) constant.Value {
 
-    stringVal := constant.StringVal(stringValue)
+	stringVal := constant.StringVal(stringValue)
 
-    switch otherValue.Kind() {
-    case constant.Int:
-        intVal, _ := constant.Int64Val(otherValue)
-        intValStr := strconv.FormatInt(intVal, 10)
-        if ordered {
-            return constant.MakeString(stringVal + intValStr)
-        } else {
-            return constant.MakeString(intValStr + stringVal)
-        }
-    case constant.Float:
-        floatVal, _ := constant.Float64Val(otherValue)
-        floatValStr := util.FloatToString(floatVal)
-        if ordered {
-            return constant.MakeString(stringVal + floatValStr)
-        } else {
-            return constant.MakeString(floatValStr + stringVal)
-        }
-    case constant.Bool:
-        boolVal := constant.BoolVal(otherValue)
-        boolValStr := strconv.FormatBool(boolVal)
-        if ordered {
-            return constant.MakeString(stringVal + boolValStr)
-        } else {
-            return constant.MakeString(boolValStr + stringVal)
-        }
-    }
+	switch otherValue.Kind() {
+	case constant.Int:
+		intVal, _ := constant.Int64Val(otherValue)
+		intValStr := strconv.FormatInt(intVal, 10)
+		if ordered {
+			return constant.MakeString(stringVal + intValStr)
+		} else {
+			return constant.MakeString(intValStr + stringVal)
+		}
+	case constant.Float:
+		floatVal, _ := constant.Float64Val(otherValue)
+		floatValStr := util.FloatToString(floatVal)
+		if ordered {
+			return constant.MakeString(stringVal + floatValStr)
+		} else {
+			return constant.MakeString(floatValStr + stringVal)
+		}
+	case constant.Bool:
+		boolVal := constant.BoolVal(otherValue)
+		boolValStr := strconv.FormatBool(boolVal)
+		if ordered {
+			return constant.MakeString(stringVal + boolValStr)
+		} else {
+			return constant.MakeString(boolValStr + stringVal)
+		}
+	}
 
-    panic("You can concatenate string with other strings, integers, floats and booleans.")
+	panic("You can concatenate string with other strings, integers, floats and booleans.")
 
 }
 
 func opsWithNums(left constant.Value, operator TokenType, right constant.Value) constant.Value {
 
-    if left.Kind() == constant.Int || right.Kind() == constant.Int {
+	if left.Kind() == constant.Int || right.Kind() == constant.Int {
 
-        leftVal, _ := constant.Int64Val(left)
-        rightVal, _ := constant.Int64Val(right)
+		leftVal, _ := constant.Int64Val(left)
+		rightVal, _ := constant.Int64Val(right)
 
-        switch operator {
-        case token.Plus:
-            return constant.MakeInt64(leftVal + rightVal)
-        case token.Minus:
-            return constant.MakeInt64(leftVal - rightVal)
-        case token.Asterisk:
-            return constant.MakeInt64(leftVal * rightVal)
-        case token.Slash:
-            return constant.MakeInt64(leftVal / rightVal)
-        case token.LtOp:
-            return constant.MakeBool(leftVal < rightVal)
-        case token.GtOp:
-            return constant.MakeBool(leftVal > rightVal)
-        case token.LeqOp:
-            return constant.MakeBool(leftVal <= rightVal)
-        case token.GeqOp:
-            return constant.MakeBool(leftVal >= rightVal)
-        case token.EqOp:
-            return constant.MakeBool(leftVal == rightVal)
-        case token.NeqOp:
-            return constant.MakeBool(leftVal != rightVal)
-        default:
-            panic("Operator " + operator.String() + " cannot be applied to two integers.")
-        }
+		switch operator {
+		case token.Plus:
+			return constant.MakeInt64(leftVal + rightVal)
+		case token.Minus:
+			return constant.MakeInt64(leftVal - rightVal)
+		case token.Asterisk:
+			return constant.MakeInt64(leftVal * rightVal)
+		case token.Slash:
+			return constant.MakeInt64(leftVal / rightVal)
+		case token.LtOp:
+			return constant.MakeBool(leftVal < rightVal)
+		case token.GtOp:
+			return constant.MakeBool(leftVal > rightVal)
+		case token.LeqOp:
+			return constant.MakeBool(leftVal <= rightVal)
+		case token.GeqOp:
+			return constant.MakeBool(leftVal >= rightVal)
+		case token.EqOp:
+			return constant.MakeBool(leftVal == rightVal)
+		case token.NeqOp:
+			return constant.MakeBool(leftVal != rightVal)
+		default:
+			panic("Operator " + operator.String() + " cannot be applied to two integers.")
+		}
 
-    }
+	}
 
-    leftVal, _ := constant.Float64Val(left)
-    rightVal, _ := constant.Float64Val(right)
+	leftVal, _ := constant.Float64Val(left)
+	rightVal, _ := constant.Float64Val(right)
 
-    switch operator {
-    case token.Plus:
-        return constant.MakeFloat64(leftVal + rightVal)
-    case token.Minus:
-        return constant.MakeFloat64(leftVal - rightVal)
-    case token.Asterisk:
-        return constant.MakeFloat64(leftVal * rightVal)
-    case token.Slash:
-        return constant.MakeFloat64(leftVal / rightVal)
-    case token.LtOp:
-        return constant.MakeBool(leftVal < rightVal)
-    case token.GtOp:
-        return constant.MakeBool(leftVal > rightVal)
-    case token.LeqOp:
-        return constant.MakeBool(leftVal <= rightVal)
-    case token.GeqOp:
-        return constant.MakeBool(leftVal >= rightVal)
-    case token.EqOp:
-        return constant.MakeBool(leftVal == rightVal)
-    case token.NeqOp:
-        return constant.MakeBool(leftVal != rightVal)
-    default:
-        panic("Operator " + operator.String() + " cannot be applied to two numbers.")
-    }
+	switch operator {
+	case token.Plus:
+		return constant.MakeFloat64(leftVal + rightVal)
+	case token.Minus:
+		return constant.MakeFloat64(leftVal - rightVal)
+	case token.Asterisk:
+		return constant.MakeFloat64(leftVal * rightVal)
+	case token.Slash:
+		return constant.MakeFloat64(leftVal / rightVal)
+	case token.LtOp:
+		return constant.MakeBool(leftVal < rightVal)
+	case token.GtOp:
+		return constant.MakeBool(leftVal > rightVal)
+	case token.LeqOp:
+		return constant.MakeBool(leftVal <= rightVal)
+	case token.GeqOp:
+		return constant.MakeBool(leftVal >= rightVal)
+	case token.EqOp:
+		return constant.MakeBool(leftVal == rightVal)
+	case token.NeqOp:
+		return constant.MakeBool(leftVal != rightVal)
+	default:
+		panic("Operator " + operator.String() + " cannot be applied to two numbers.")
+	}
 
 }
